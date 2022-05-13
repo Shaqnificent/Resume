11ed962a692e9b95bfefa3d4f3ddbf9aeb49953f
Shaqnificent <shaqnificent@gmail.com>
2022-05-12 21:00:27 -0500

    inititalize the project files

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge:[],
+  darkmode: false,
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#17171F',
+        'selected-text': '#A3A3FF',
+        'theme': '#3F3FFF',
+        'nav': '#404053',
+        'secondary': '#9191A4',
+        'badge': '#3F3F51',
+        'input-broder': '#565666',
+        'input': '#2A2A35',
+      },
+      fontFamily: {
+        'popins':["'Poppins", 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  
+}
